61ddaaa4c59b8c9666d320dacb663a8506928881
xudongdong <xudogn@163.com>
2021-05-27 17:52:22 +0800

    Initial Commit

---
+++ NoteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoteBook
+//
+//  Created by Cat on 2021/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
